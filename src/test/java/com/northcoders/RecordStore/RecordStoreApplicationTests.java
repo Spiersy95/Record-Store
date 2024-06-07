6538331cb03ce0547623a5e00c8b90d1ba822cc3
Spiersy95 <scottspeirsstirling@gmail.com>
2024-06-07 10:09:46 +0100

    first commit

---
+++ src/test/java/com/northcoders/RecordStore/RecordStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.RecordStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
